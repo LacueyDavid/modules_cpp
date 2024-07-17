3cf5c74bd1af4265f1254fa91713004ba0a5742c
Haru Vulpes <Vulpes.Haru@gmail.com>
2024-07-17 21:39:22 +0200

    ok

--- module_7/ex02/main.cpp
+++ module_7/ex02/main.cpp
@@ -4,6 +4,24 @@
 int main()
 {
 	Array<int> array(10);
-	// for (std::size_t i = 0; i < 10; ++i)
-	// 	std::cout << array << "\n";
+	for (std::size_t i = 0; i < 10; ++i)
+	{
+		array[i] = i;
+		std::cout << array[i] << "\n";
+	}
+
+	Array<std::string> arraya(10);
+	for (std::size_t i = 0; i < 10; ++i)
+	{
+		arraya[i] = "salut";
+		std::cout << arraya[i] << "\n";
+	}
+	try
+	{
+		array[100] = 12;
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "i catch: " << e.what() << "\n";
+	}
 }
