fd04348dc48bfb97b8e54c0b4a01ccd0f71dfa3c
Shiro Vulpes <Vulpes.Shiro@gmail.com>
2024-09-21 00:02:27 +0200

    ok

--- module_9/ex01/RPN.cpp
+++ module_9/ex01/RPN.cpp
@@ -1,23 +1,161 @@
 #include "RPN.hpp"
+/// clean function
 
-// private
-
-RPN::RPN(){}
-
-RPN::RPN(const RPN &other __attribute__((unused))){}
-
-RPN &RPN::operator=(const RPN &other __attribute__((unused))){ return *this; }
-
-// public
-
-RPN::RPN(const std::string &input)
-	: input(input)
+long int RPN::charToLongInt(char charValue)
 {
-	std::cout << "constructor called\n";
-	std::cout << input << "\n";
+	return charValue - '0';
 }
 
-RPN::~RPN()
+//// Struct StackElement
+
+StackElement::StackElement() {}
+
+StackElement::StackElement(const StackElement& other)
+	: type(other.type)
 {
-	std::cout << "destructor called\n";
+	intValue = 0;
+	charValue = 0;
+	if (type == INT)
+		intValue = other.intValue;
+	else if (type == CHAR)
+		charValue = other.charValue;
+}
+
+StackElement& StackElement::operator=(const StackElement& other)
+{
+	if (this == &other)
+		return *this;
+	type = other.type;
+	intValue = 0;
+	charValue = 0;
+	if (type == INT)
+		intValue = other.intValue;
+	else if (type == CHAR)
+		charValue = other.charValue;
+	return *this;
+}
+
+StackElement::~StackElement() {}
+
+StackElement::StackElement(long int value)
+: type(INT)
+	, intValue(value)
+{}
+
+StackElement::StackElement(char value)
+: type(CHAR)
+	, charValue(value)
+{}
+
+///// class
+
+/// canonical form
+
+RPN::RPN() {}
+
+RPN::RPN(const RPN& other __attribute__((unused))) {}
+
+RPN& RPN::operator=(const RPN& other __attribute__((unused))) { return *this; }
+
+RPN::~RPN() {}
+
+//// class functions
+
+bool RPN::isOperator(const StackElement& elem) const
+{
+	return elem.type == CHAR;
+}
+
+void RPN::validateInput(const std::string& input) const
+{
+	for (size_t i = 0; i < input.size(); ++i)
+	{
+		char ch = input[i];
+		if (i % 2 == 1 && ch != ' ')
+			throw std::invalid_argument("Error");
+		if (i % 2 == 0 && !isdigit(ch) && ch != '+' && ch != '-' && ch != '*' && ch != '/')
+			throw std::invalid_argument("Error");
+	}
+}
+
+void RPN::executeCalculation()
+{
+	if (operandStack.size() < 2)
+		throw std::invalid_argument("Error");
+
+	char op = rpnStack.top().charValue;
+	rpnStack.pop();
+
+	long int b = operandStack.top().intValue;
+	operandStack.pop();
+
+	long int a = operandStack.top().intValue;
+	operandStack.pop();
+
+	operandStack.push(performOperation(a, b, op));
+}
+
+void RPN::processOperand()
+{
+	if (rpnStack.empty())
+		throw std::invalid_argument("Error");
+
+	operandStack.push(rpnStack.top());
+	rpnStack.pop();
+}
+
+long int RPN::performOperation(long int a, long int b, char op) const
+{
+	switch (op)
+	{
+		case '+': return a + b;
+		case '-': return a - b;
+		case '*': return a * b;
+		case '/':
+			if (b == 0)
+				throw std::invalid_argument("Error");
+			return a / b;
+		default: throw std::invalid_argument("Error");
+	}
+}
+
+
+long int RPN::getFinalResult() const
+{
+	if (operandStack.size() != 1)
+		throw std::invalid_argument("Error");
+
+	long int result = operandStack.top().intValue;
+	if (result > INT_MAX || result < INT_MIN)
+		throw std::invalid_argument("Error");
+
+	return result;
+}
+
+void RPN::pushElementToStack(char charValue)
+{
+	if (isdigit(charValue))
+		rpnStack.push(charToLongInt(charValue));
+	else
+		rpnStack.push(charValue);
+}
+
+void RPN::parseInputToStack(const std::string& input)
+{
+	for (int i = input.size() - 1; i >= 0; --i) {
+		if (i % 2 == 0)
+			pushElementToStack(input[i]);
+	}
+}
+
+long int RPN::evaluateRPN(const std::string& input)
+{
+	validateInput(input);
+	parseInputToStack(input);
+	while (!rpnStack.empty()) {
+		isOperator(rpnStack.top())
+			? executeCalculation()
+			: processOperand();
+	}
+	return getFinalResult();
 }
--- module_9/ex01/RPN.hpp
+++ module_9/ex01/RPN.hpp
@@ -1,67 +1,67 @@
-// #ifndef RPN_HPP
-// # define RPN_HPP
-//
-// #include <string>
-// #include <iostream>
-// #include <cstdlib>
-// #include <sstream>
-// #include <stack>
-//
-// enum Type
-// {
-// 	INT,
-// 	CHAR
-// };
-//
-// class RPN
-// {
-// private:
-// 	std::string input;
-// 	RPN();
-// 	RPN(const RPN &other);
-// 	RPN &operator=(const RPN &other);
-// public:
-// 	RPN(const std::string &input);
-// 	~RPN();
-// };
-//
-// struct StackElement
-// {
-// 	Type type;
-//
-// 	union
-// 	{
-// 		int intValue;
-// 		char charValue;
-// 	};
-//
-// 	StackElement(int value) : type(INT), intValue(value) {}
-// 	StackElement(char value) : type(CHAR), charValue(value) {}
-// 	StackElement(const StackElement& other)
-// 		: type(other.type)
-// 	{
-// 		if (type == INT)
-// 			intValue = other.intValue;
-// 		else if (type == CHAR)
-// 			charValue = other.charValue;
-// 	}
-// 	StackElement &operator=(const StackElement &other)
-// 	{
-// 		if (&other != this)
-// 			return *this;
-// 		type = other.type;
-// 		if (type == INT)
-// 		{
-// 			intValue = other.intValue;
-// 			charValue = 0;
-// 		}
-// 		else if (type == CHAR)
-// 		{
-// 			charValue = other.charValue;
-// 			intValue = 0;
-// 		}
-// 		return *this;
-// 	}
-// };
-//
-// #endif
+#ifndef RPN_HPP
+# define RPN_HPP
+
+#include <climits>
+#include <iostream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <cctype>
+#include <cstdlib>
+
+enum Type { INT, CHAR };
+
+struct StackElement
+{
+public:
+	Type type;
+
+	union
+	{
+		long int intValue;
+		char charValue;
+	};
+
+	StackElement(long int value);
+	StackElement(char value);
+
+	StackElement& operator=(const StackElement& other);
+	StackElement(const StackElement& other);
+	~StackElement();
+private:
+	StackElement();
+};
+
+class RPN
+{
+public:
+	long int evaluateRPN(const std::string& input);
+
+	RPN();
+	~RPN();
+
+private:
+	std::stack<StackElement> rpnStack;
+	std::stack<StackElement> operandStack;
+
+	long int charToLongInt(char charValue);
+	void validateInput(const std::string& input) const;
+	bool isOperator(const StackElement& elem) const;
+	void validateCharacter(char ch) const;
+	void validateInputCharacters(const std::string& input) const;
+	void validateSpacingInInput(const std::string& input) const;
+	long int performOperation(long int a, long int b, char op) const;
+	void executeCalculation();
+	void processOperand();
+	void processOperator();
+	bool isFinalResultReady() const;
+	bool hasTooManyOperands() const;
+	long int getFinalResult() const;
+	void pushElementToStack(char charValue);
+	void parseInputToStack(const std::string& input);
+
+	RPN(const RPN& other);
+	RPN& operator=(const RPN& other);
+};
+
+#endif
